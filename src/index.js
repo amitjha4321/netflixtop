944d03eff03c5bad592a5eca87c253cac6f99636
Amit Jha <amitjha4321@gmail.com>
2021-01-09 21:45:56 +0545

    first card component added and used

--- src/index.js
+++ src/index.js
@@ -1,21 +1,43 @@
 import React from 'react';
 import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <>
-  <div className="cards">
+//in this we are using functional component lateer somewhere we will use class component...
+//always first letter of the function should be in capital...
+function Card(){
+<>
+  { <div className="cards">
     <div className="card">
-      <img src="" alt="myPic" className="card__img"/>
+      <img src="https://wallpapercave.com/wp/wp4056410.jpg" alt="myPic" className="card__img"/>
       <div className="card__info">
         <span className="card__category">A Netflix Original Series</span>
-        <h3 className="card_title"></h3>
-        <a href="" target="_blank"></a>
+        <h3 className="card_title">DARK</h3>
+        <a href="https://www.netflix.com/np/title/80100172" target="_blank">
           <button>Watch Now</button>
-
+        </a>
       </div>
 
     </div>
-  </div>
+  </div> }
+  </>
+}
 
-  </>, document.getElementById('root')
+ReactDOM.render(
+  <Card/>,document.getElementById('root')
+  // <>
+  // {/* <div className="cards">
+  //   <div className="card">
+  //     <img src="https://wallpapercave.com/wp/wp4056410.jpg" alt="myPic" className="card__img"/>
+  //     <div className="card__info">
+  //       <span className="card__category">A Netflix Original Series</span>
+  //       <h3 className="card_title">DARK</h3>
+  //       <a href="https://www.netflix.com/np/title/80100172" target="_blank">
+  //         <button>Watch Now</button>
+  //       </a>
+  //     </div>
+
+  //   </div>
+  // </div> */}
+  // {/* <input type='text' value='amit'></input> */}
+
+  // </>, document.getElementById('root')
 );
