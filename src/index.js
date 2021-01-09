edd4662b60435236839f267ebbc577b099d61bd8
Amit Jha <amitjha4321@gmail.com>
2021-01-09 21:04:12 +0545

    started

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ ReactDOM.render(
     <div className="card">
       <img src="" alt="myPic" className="card__img"/>
       <div className="card__info">
-        <span className="card__category"></span>
+        <span className="card__category">A Netflix Original Series</span>
         <h3 className="card_title"></h3>
         <a href="" target="_blank"></a>
           <button>Watch Now</button>
